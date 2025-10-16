ccf46a71d09e34676da4726619b5a9851df5dc50
AGBO Martin <letcode.dev@gmail.com>
2025-10-16 19:48:16 +0000

    Update actions.ts

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -112,7 +112,7 @@ export async function registerUserAction(formData: FormData) {
   })
 
   revalidatePath('/')
-  redirect('/signin')
+  redirect('/login')
 }
 
   export const handleSignOut = async () => {
